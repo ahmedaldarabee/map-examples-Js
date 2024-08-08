8cfd8268596a40f57c5202fa94c3a4a9420d6f1d
Ahmed Nail Al Drabea <124718632+ahmedaldrabea@users.noreply.github.com>
2024-08-08 15:37:07 +0300

    Add files via upload

---
+++ map.js
@@ -0,0 +1,26 @@
+function mapExampleOne(){
+    let name = "ahmed-NAYEL";
+    let swappingLetter = name.split("").map((ele)=>{
+        // conditional operator
+            return ele == ele.toUpperCase() ? ele.toLowerCase() : ele.toUpperCase();
+    }).join("");
+    console.log(swappingLetter)
+}
+
+function mapExampleTwo(){
+    let arrOFNumbers = [11,12,-13,14,-5];
+    let invertNumber = arrOFNumbers.map((ele)=>{
+        return -ele;
+    })
+    console.log(invertNumber)
+}
+
+function mapExampleThree(){
+    let name = "A7m3d";
+    name = name.split("").map((ele) => {
+       return isNaN(parseInt(ele)) ? ele : "";
+    }).join("");
+
+    console.log(name);
+}
+mapExampleThree()
